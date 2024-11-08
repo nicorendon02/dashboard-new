73fb7b3c8cf78854576ae97546ce71b39adbc145
nico <nicorendon02@gmail.com>
2024-11-08 16:41:02 -0500

    test data temp & humid

--- src/app/sensor-charts/sensor-charts.component.ts
+++ src/app/sensor-charts/sensor-charts.component.ts
@@ -51,7 +51,18 @@ export class SensorChartsComponent {
     // Replace with actual logic to load data from your text file
     return [
       { timestamp: new Date('2024-11-01T00:00:00Z'), temperature: 20, humidity: 50 },
-      { timestamp: new Date('2024-11-01T01:00:00Z'), temperature: 21, humidity: 52 },
+      { timestamp: new Date('2024-11-01T00:01:00Z'), temperature: 21, humidity: 51 },
+      { timestamp: new Date('2024-11-01T00:02:00Z'), temperature: 19, humidity: 52 },
+      { timestamp: new Date('2024-11-01T00:03:00Z'), temperature: 15, humidity: 63 },
+      { timestamp: new Date('2024-11-01T00:04:00Z'), temperature: 24, humidity: 54 },
+      { timestamp: new Date('2024-11-01T00:05:00Z'), temperature: 25, humidity: 45 },
+      { timestamp: new Date('2024-11-01T00:06:00Z'), temperature: 26, humidity: 56 },
+      { timestamp: new Date('2024-11-01T00:07:00Z'), temperature: 20, humidity: 57 },
+      { timestamp: new Date('2024-11-01T00:08:00Z'), temperature: 28, humidity: 58 },
+      { timestamp: new Date('2024-11-01T00:09:00Z'), temperature: 29, humidity: 59 },
+      { timestamp: new Date('2024-11-01T00:10:00Z'), temperature: 20, humidity: 60 },
+
+
       // Add more sample data or load dynamically
     ];
   }
